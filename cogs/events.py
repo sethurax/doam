059816ac0be73a6dbc5fed322b492dc5b9098b57
Seth Morgan <seth@sethmorgan.io>
2026-04-02 16:00:37 -0700

    get rid of bytecode generation, downgrade to py 3.13 to bypass 3.14 event loop changes

--- cogs/events.py
+++ cogs/events.py
@@ -11,23 +11,15 @@ class EventsCog(c.Cog):
     async def on_ready(self):
         await self.bot.change_presence(status=d.Status.online, activity=d.Game("doam."))
 
-        if self.bot is d.ClientUser:
-            print(
-                f"{time.strftime('%H:%M:%S', time.localtime())}: Logged in as {self.bot.user} (ID: {self.bot.user.id})"
-            )
-        else:
-            print(
-                f"{time.strftime('%H:%M:%S', time.localtime())}: Logged in; client information unknown."
-            )
+        print(
+            f"{time.strftime('%H:%M:%S', time.localtime())}: Logged in as {self.bot.user} (ID: {self.bot.user.id})"
+        )
 
     @c.Cog.listener()
     async def on_application_command(self, ctx: d.ApplicationContext):
-        if ctx.command is d.ApplicationCommand:
-            print(
-                f"{time.strftime('%H:%M:%S', time.localtime())}: Command: /{ctx.command.qualified_name} | Guild: {ctx.guild.name if ctx.guild else 'DM'} | User: {ctx.author.display_name}"
-            )
-        else:
-            print(f"{time.strftime('%H:%M:%S', time.localtime())}: Command: unknown")
+        print(
+            f"{time.strftime('%H:%M:%S', time.localtime())}: Command: /{ctx.command.qualified_name} | Guild: {ctx.guild.name if ctx.guild else 'DM'} | User: {ctx.author.display_name}"
+        )
 
     @c.Cog.listener()
     async def on_guild_join(self, guild: d.Guild):
@@ -35,10 +27,8 @@ class EventsCog(c.Cog):
             f"{time.strftime('%H:%M:%S', time.localtime())}: Bot joined to new guild - {guild.name} ({guild.id})"
         )
 
-        if self.bot.owner_id is int:
-            owner = self.bot.get_user(self.bot.owner_id)
-            if owner is d.User:
-                await owner.send(f"Bot joined to new guild: {guild.name} ({guild.id})")
+        owner = self.bot.get_user(self.bot.owner_id)
+        await owner.send(f"Bot joined to new guild: {guild.name} ({guild.id})")
 
 
 def setup(bot: d.Bot):
