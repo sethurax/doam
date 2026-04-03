e986ca883fecd117dd5bc83e2e6c25720e7dd77a
Seth Morgan <seth@sethmorgan.io>
2026-04-03 11:21:40 -0700

    Update redis config to use db0 for both prod and dev

--- db.py
+++ db.py
@@ -1,14 +1,11 @@
 import os
 import redis
 
-# Using two separate DBs on one Redis instance for production and development
-branch = 0 if os.getenv("BRANCH") == "main" else 1
-
 db = redis.Redis(
     host=os.getenv("REDIS_HOST_PROD", ""),
     port=int(os.getenv("REDIS_PORT", "18224")),
     username=os.getenv("REDIS_UNAME", "default"),
     password=os.getenv("REDIS_PWD", "default"),
-    db=branch,
+    db=0,
     decode_responses=True,
 )
