cb655792abdac5ad0e73423fb521756015a4e851
Seth Morgan <seth@sethmorgan.io>
2026-04-03 10:54:17 -0700

    Update db to use dynamic redis port from env

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ branch = 0 if os.getenv("BRANCH") == "main" else 1
 
 db = redis.Redis(
     host=os.getenv("REDIS_HOST_PROD", ""),
-    port=18224,
+    port=int(os.getenv("REDIS_PORT", "18224")),
     username=os.getenv("REDIS_UNAME", "default"),
     password=os.getenv("REDIS_PWD", "default"),
     db=branch,
